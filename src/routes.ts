a4394350e3c6b8718e8311b060357db942bab9be
Cristobal Vasquez <cristobal.vasquez.m@usach.cl>
2024-11-07 22:02:52 -0300

    routing for nginx fixed

--- src/routes.ts
+++ src/routes.ts
@@ -2,70 +2,70 @@ import { MainController } from "./controller/MainController"
 
 export const Routes = [{
     method: "get",
-    route: "/api/v1/articles",
+    route: "/articles",
     controller: MainController,
     action: "articleAll"
 }, {
     method: "get",
-    route: "/api/v1/articles/:id",
+    route: "/articles/:id",
     controller: MainController,
     action: "articleOne"
 }, {
     method: "post",
-    route: "/api/v1/articles",
+    route: "/articles",
     controller: MainController,
     action: "articleSave"
 }, {
     method: "delete",
-    route: "/api/v1/articles/:id",
+    route: "/articles/:id",
     controller: MainController,
     action: "articleRemove"
 }, {
     method: "patch",
-    route: "/api/v1/articles/:id",
+    route: "/articles/:id",
     controller: MainController,
     action: "articleUpdate"
 }, {
     method: "get",
-    route: "/api/v1/food-advice",
+    route: "/food-advice",
     controller: MainController,
     action: "foodAdviceAll"
 }, {
     method: "get",
-    route: "/api/v1/food-advice/:id",
+    route: "/food-advice/:id",
     controller: MainController,
     action: "foodAdviceOne"
 }, {
     method: "post",
-    route: "/api/v1/food-advice",
+    route: "/food-advice",
     controller: MainController,
     action: "foodAdviceSave"
 }, {
     method: "delete",
-    route: "/api/v1/food-advice/:id",
+    route: "/food-advice/:id",
     controller: MainController,
     action: "foodAdviceRemove"
 }, {
     method: "patch",
-    route: "/api/v1/food-advice/:id",
+    route: "/food-advice/:id",
     controller: MainController,
     action: "foodAdviceUpdate"
 },
 {
     method: "get",
-    route: "/api/v1/expert-profile",
+    route: "/expert-profile",
     controller: MainController,
     action: "expertsAll"
 },
 {
     method: "get",
-    route: "/api/v1/expert-profile/byUserId/:id",
+    route: "/expert-profile/byUserId/:id",
     controller: MainController,
     action: "expertsOneByUserId"
 },
 {
     method: "get",
-    route: "/api/v1/expert-profile/byId/:id",
+    route: "/expert-profile/byId/:id",
     controller: MainController,
     action: "expertsOne"
 },]
